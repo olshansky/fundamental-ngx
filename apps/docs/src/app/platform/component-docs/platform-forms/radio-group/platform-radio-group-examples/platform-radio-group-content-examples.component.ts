a4fd9beff462113cbe43e168209006b52b962897
Deepak Kumar <deepak.kumar14@sap.com>
2020-05-25 02:22:31 +0530

    fix: docs change and func return

--- apps/docs/src/app/platform/component-docs/platform-forms/radio-group/platform-radio-group-examples/platform-radio-group-content-examples.component.ts
+++ apps/docs/src/app/platform/component-docs/platform-forms/radio-group/platform-radio-group-examples/platform-radio-group-content-examples.component.ts
@@ -3,23 +3,33 @@ import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
     selector: 'fdp-platform-radio-group-content-example',
-    templateUrl: './platform-radio-group-content-example.component.html'
+    templateUrl: './platform-radio-group-content-example.component.html',
+    styleUrls: ['platform-radio-group.component.scss']
 })
 export class PlatformRadioGroupContentExampleComponent implements DoCheck {
     favoriteSeason: string = '';
-    favoriteSeason2: string = '';
+    favoriteSeason2: string = 'spring';
     favoriteMonth: string = '';
     seasons: string[] = ['Winter', 'Spring', 'Summer', 'Autumn'];
 
-    customForm = new FormGroup({
-        example1: new FormControl({ value: '', disabled: false }),
-        example2: new FormControl({ value: '', disabled: false }),
-        example3: new FormControl({ value: 'Winter', disabled: false }),
+    form1 = new FormGroup({
+        example1: new FormControl({ value: '', disabled: false })
+    });
+
+    form2 = new FormGroup({
+        example2: new FormControl({ value: '', disabled: false })
+    });
+
+    form3 = new FormGroup({
+        example3: new FormControl({ value: 'winter', disabled: false })
+    });
+
+    form4 = new FormGroup({
         example4: new FormControl({ value: '', disabled: false })
     });
 
     ngDoCheck() {
-        this.customForm.get('example4').setErrors({ invalid: true });
-        this.customForm.get('example4').markAsTouched();
+        this.form4.get('example4').setErrors({ invalid: true });
+        this.form4.get('example4').markAsTouched();
     }
 }
